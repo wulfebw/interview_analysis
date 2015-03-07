5946a59d0c7d3a062431ed17256c892542fa8032
Blake Wulfe <blake.w.wulfe@gmail.com>
2015-03-06 21:06:43 -0600

    interview analysis initial

---
+++ analyzers/linguistic_analysis.py
@@ -0,0 +1,113 @@
+"""
+filename: linguistic_analysis.py 
+description: The functions in this file do the following:
+
+	- recognize speech from an audio file
+	- analyze language in text format 
+		- get most common word
+		- get most common bigram
+		- get number of times a specific word was said
+
+"""
+
+import os
+import sys
+import nltk
+import sphinxbase
+import pocketsphinx
+
+"""
+Recognize Speech from audio files.
+"""
+
+def recognize_speech(filename,
+					hmm = '../speech_models/cmusphinx-5prealpha-en-us-2.0',
+					lm = '../speech_models/cmusphinx-5.0-en-us.lm.dmp',
+					dic = '../speech_models/hub4.5000.dic',
+					samp_rate = '16000'):
+	"""
+	Recognizes speech in a wave file.
+
+	:type hmm: string
+	:param hmm: the hidden markov model file location
+
+	:type lm: string
+	:param lm: the language model file location
+
+	:type dic: string
+	:param dic: the dictionary file location
+
+	:type samp_rate: string
+	:param samp_rate: the sample rate at which the speech recognition models were trained
+
+	:rtype: string
+	:return: the speech in the provided audio file
+	"""
+
+	speech_recognizer = pocketsphinx.Decoder(hmm=hmm, lm=lm, dict=dic, samprate=samp_rate)
+	# open the wave file
+	wavfile = file(filename, 'r')
+	wavfile.seek(50)
+	# decode speech in the wave file
+	speech_recognizer.decode_raw(wavfile)
+	# return the hypothesis of the recognizer. 
+	result = speech_recognizer.get_hyp()
+	# actual result is a tuple and we only want the decoded speech (first element in tuple)
+	return result[0]
+
+"""
+Extract speech features.
+"""
+
+def get_most_common_word(tokens):
+	""" 
+	Gets the most common word. 
+
+	:type tokens: list of strings
+	:param tokens: list of the words in speech
+
+	:rtype: string
+	:return: the most common word in the speech
+	"""
+
+	freq_dist = nltk.FreqDist(tokens)
+	return freq_dist.most_common(1)
+
+def get_most_common_bigram(tokens):
+	""" 
+	Gets the most common bigram. 
+
+	:type tokens: list of strings
+	:param tokens: list of the words in speech
+
+	:rtype: string
+	:return: the most common bigram in the speech
+	"""
+
+	bigrams = list(nltk.bigrams(tokens))
+	freq_dist_bigrams = nltk.FreqDist(bigrams)
+	return req_dist_biagrams.most_common(1)
+
+def get_num_occurences_of_word(tokens, word='i'):
+	""" 
+	Gets the number of occurences of a word, with default as 'I'. 
+
+	:type tokens: list of strings
+	:param tokens: list of the words in speech
+
+	:type word: string
+	:param word: the word to retrieve the count of
+	"""
+	
+	return tokens.count(word)
+
+def get_linguistic_features(speech):
+	""" Calculates all linguistic features and then returns a dictionary of labels to features. """
+
+	tokens = nltk.word_tokenize(speech)
+	linguistic_features = dict()
+	linguistic_features['speech'] = speech
+	linguistic_features['most common word'] = get_most_common_word(tokens)
+	linguistic_features['most common bigram'] = get_most_common_bigram(tokens)
+	linguistic_features['number of occurences of {0}'.format('i')] = get_num_occurences_of_word(tokens, 'i')
+	return linguistic_features 
---
+++ upload_analysis.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+from werkzeug.contrib.fixers import ProxyFix
+from analyzers.linguistic_analysis import recognize_speech, get_linguistic_features
+from analyzers.utils import *
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'iterviews/'
+
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['wav', 'webm'])
+
+# Set the route to the file upload
+@app.route('/')
+def index():
+    return render_template('recordrtc_index.html')
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+
+	# get a random value to indentify this interview
+	# not using a database for now, really need to learn more flask
+	pk_length = 10
+	pk = get_rand_hex_value(pk_length)
+	make_dirs(pk)
+
+	# create a dictionary to hold the features we collect
+	rtn_features = dict()
+	# init the audio and video files to be returned
+	video_filename = ''
+	audio_filename = ''
+
+	# video upload
+	try:
+		video = request.files['video-blob']
+	except Exception as e:
+		video = None
+		# raise Exception(e)
+
+	if video:
+		# dir/filename for video to be saved as
+		video_filename = os.path.join(app.config['UPLOAD_FOLDER'],'{0}/video.webm'.format(pk))
+		# save the video
+		video.save(video_filename)
+
+	try:
+		audio = request.files['audio-blob']
+	except Exception as e:
+		audio = None
+		# raise Exception(e)
+
+	if audio:
+		# dir/filename for audio to be saved as
+		audio_filename = os.path.join(app.config['UPLOAD_FOLDER'],'{0}/audio.wav'.format(pk))
+		# save the audio 
+		audio.save(audio_filename)
+		# convert to mono
+		audio_mono_filename = stereo_to_mono(audio_filename)
+		# perform speech recognition
+		speech = recognize_speech(audio_mono_filename)
+		# get the linguistic stats 
+		linguistic_features = get_linguistic_features(speech)
+		# get output file for linguistic features
+		linguistic_features_file = 'interviews/{0}/features/linguistic_features.txt'
+		# write linguistic features to file
+		write_dict_features_to_file(linguistic_features, linguistic_features_file)
+		# add linguistic features to rtn_dict
+		rtn_features = dict(rtn_features + linguistic_features)
+
+	# render the template with the collected features
+	return render_template('features.html', 
+							features=rtn_features, 
+							video_filename=video_filename, 
+							audio_filename=audio_filename, 
+							pk=pk)
+
+# route that will display previously uploaded media
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=int("80"), debug=True)
+
